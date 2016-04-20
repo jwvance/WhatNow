ba2918850fd08abfd7578efd93edef8c0c9050a6
Charly <caaespin@ucsc.edu>
2016-04-20 12:58:55 -0700

    added button for saving and testing events

--- WhatNow/app/src/main/java/software_engineering/whatnow/Test.java
+++ WhatNow/app/src/main/java/software_engineering/whatnow/Test.java
@@ -1,7 +1,9 @@
 package software_engineering.whatnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,5 +13,11 @@ public class Test extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
+	}
+
+	public void submitAppointment(View v) {
+		Intent intent = new Intent(Intent.ACTION_INSERT);
+		intent.setType("vnd.android.cursor.item/event");
+		startActivity(intent);
 	}
 }
