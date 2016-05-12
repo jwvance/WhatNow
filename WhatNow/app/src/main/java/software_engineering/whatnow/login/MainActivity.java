8134ade9f33a5b456e3d3ac83341efaa38a398a8
Vanessa <vahurtad@ucsc.edu>
2016-05-11 19:16:27 -0700

    multDex enabled, login implementation

---
+++ WhatNow/app/src/main/java/software_engineering/whatnow/login/MainActivity.java
@@ -0,0 +1,83 @@
+package software_engineering.whatnow.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import software_engineering.whatnow.MyProfileActivity;
+import software_engineering.whatnow.R;
+import software_engineering.whatnow.TabActivity;
+import software_engineering.whatnow.firebase_stuff.Constants;
+import software_engineering.whatnow.login.BaseActivity;
+import software_engineering.whatnow.model.User;
+import software_engineering.whatnow.utils.*;
+
+public class MainActivity extends BaseActivity {
+    private Firebase mUserRef;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private ValueEventListener mUserRefListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.setTitle("My Profile");
+
+        /**
+         * Create Firebase references
+         */
+        mUserRef = new Firebase(Constants.FIREBASE_URL_USERS).child(mEncodedEmail);
+
+        /**
+         * Add ValueEventListeners to Firebase references
+         * to control get data and control behavior and visibility of elements
+         */
+        mUserRefListener = mUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                /**
+                 * Set the activity title to current user name if user is not null
+                 */
+                if (user != null) {
+                    /* Assumes that the first word in the user's name is the user's first name. */
+                    String firstName = user.getName().split("\\s+")[0];
+                    String title = "Hello " + firstName;
+                    setTitle(title);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e(LOG_TAG,
+                        getString(R.string.log_error_the_read_failed) +
+                                firebaseError.getMessage());
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUserRef.removeEventListener(mUserRefListener);
+    }
+//
+//    public void callNewActivity (View view) {
+//        startActivity(new Intent(this, TabActivity.class));
+//    }
+
+
+}
---
+++ WhatNow/app/src/main/java/software_engineering/whatnow/login/base/FacebookLoginActivity.java
@@ -0,0 +1,12 @@
+package software_engineering.whatnow.login.base;
+import android.widget.TextView;
+
+import com.facebook.login.widget.LoginButton;
+
+public class FacebookLoginActivity {
+
+    private TextView info;
+    private LoginButton loginButton;
+
+
+}
