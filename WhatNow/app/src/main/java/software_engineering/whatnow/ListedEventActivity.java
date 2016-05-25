e6025e88d745e0e69e3f58dd428a41fd423f327b
Vanessa <vahurtad@ucsc.edu>
2016-05-24 18:12:56 -0700

    gallery stuff

--- WhatNow/app/src/main/java/software_engineering/whatnow/ListedEventActivity.java
+++ WhatNow/app/src/main/java/software_engineering/whatnow/ListedEventActivity.java
@@ -11,15 +11,18 @@
 
 package software_engineering.whatnow;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -30,12 +33,19 @@ import android.util.Base64;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import software_engineering.whatnow.firebase_stuff.Constants;
@@ -57,6 +67,21 @@ public class ListedEventActivity extends AppCompatActivity {
 	private RecyclerView recyclerView;
 	private TextView pastHostEventsText;
 	private Firebase firebaseEvents;
+
+
+	private static final int RESULT_LOAD_IMG = 1;
+	//	private Integer imagesGal[];
+	private String imagePath;
+	private Bitmap imageG;
+	private String imgArr[];
+	private String imageAsString;
+	private boolean onGalleryclick;
+	private	ArrayList<Bitmap> galArray;
+	private	ArrayList<ImageView> viewArray;
+
+	boolean isImageFit;
+
+	HorizontalScrollView hsv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -134,6 +159,13 @@ public class ListedEventActivity extends AppCompatActivity {
 		recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 		recyclerView.setAdapter(recyclerAdapter);
+
+
+		galArray = new ArrayList<Bitmap>(100);
+
+		//hide scroll bar
+		HorizontalScrollView horizontalScrollView = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
+		horizontalScrollView.setHorizontalScrollBarEnabled(false);
 	}
 
 	public void searchMap(View view){
@@ -202,5 +234,159 @@ public class ListedEventActivity extends AppCompatActivity {
 		//call to
 
 	}
+
+	public void chooseImageGal(View v){
+		onGalleryclick = false;
+		// Create intent to Open Image applications like Gallery, Google Photos
+		Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		// Start the Intent
+		startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+			Uri uri = data.getData();
+
+			try {
+				String boh= "";
+				grantUriPermission(boh, uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+				imagePath = getRealPathFromURI(this, uri);
+				imageG = scaleImage(this, uri);
+				Log.wtf("IMAGE", imagePath);
+
+				ByteArrayOutputStream stream = new ByteArrayOutputStream();
+				imageG.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+				byte[] byteArray = stream.toByteArray();
+				imageAsString = android.util.Base64.encodeToString(byteArray, android.util.Base64.DEFAULT);
+
+				//add images to gallery
+//				ImageView imageView = (ImageView) findViewById(R.id.image_display_gallery);
+				galArray.add(imageG);
+				Log.wtf("IMAGE Array ", galArray.toString());
+
+				LinearLayout imageGallery = (LinearLayout)findViewById(R.id.my_gallery);
+				for(Bitmap image: galArray){
+					imageGallery.addView(getImages(image));
+//					imageView.setImageBitmap(imageG);
+				}
+				//clear array
+				galArray.clear();
+				Log.wtf("IMAGE Array ", "clear");
+
+				((Button) findViewById(R.id.choose_gallery)).setText("Add image");
+			} catch (IOException e) {
+				e.printStackTrace();
+				Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+
+	private View getImages(Bitmap image) {
+		final ImageView imageView = new ImageView(getApplicationContext());
+//		RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)imageView.getLayoutParams();
+//		params.addRule(RelativeLayout.BELOW, R.id.choose_gallery);
+//		imageView.setLayoutParams(params);
+//
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		lp.setMargins(0,0,0,0);
+		imageView.setLayoutParams(lp);
+		imageView.setPadding(10,0,10,0);
+
+		imageView.setImageBitmap(image);
+		imageView.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v)
+			{
+				//go to gallery activity
+//				if(isImageFit) {
+//					isImageFit=false;
+//					LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//					imageView.setLayoutParams(lp);
+//					imageView.setAdjustViewBounds(true);
+//				}else{
+//					isImageFit=true;
+//					LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+//					imageView.setLayoutParams(lp);
+//					imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+//				}
+			}
+		});
+
+		return imageView;
+	}
+
+
+	public String getRealPathFromURI(Context context, Uri contentUri) {
+		Cursor cursor = null;
+		try {
+			String[] proj = { MediaStore.Images.Media.DATA };
+			cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			cursor.moveToFirst();
+			return cursor.getString(column_index);
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+	}
+
+	public static Bitmap scaleImage(Context context, Uri photoUri) throws IOException {
+		InputStream is = context.getContentResolver().openInputStream(photoUri);
+		BitmapFactory.Options dbo = new BitmapFactory.Options();
+		dbo.inJustDecodeBounds = true;
+		BitmapFactory.decodeStream(is, null, dbo);
+		is.close();
+
+		int rotatedWidth, rotatedHeight;
+		int orientation = 0;
+
+
+		rotatedWidth = dbo.outWidth;
+		rotatedHeight = dbo.outHeight;
+
+
+		Bitmap srcBitmap;
+		is = context.getContentResolver().openInputStream(photoUri);
+		if (rotatedWidth > 400 || rotatedHeight > 400) {
+			float widthRatio = ((float) rotatedWidth) / ((float) 400);
+			float heightRatio = ((float) rotatedHeight) / ((float) 400);
+			float maxRatio = Math.max(widthRatio, heightRatio);
+
+			// Create the bitmap from file
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = (int) maxRatio;
+			srcBitmap = BitmapFactory.decodeStream(is, null, options);
+		} else {
+			srcBitmap = BitmapFactory.decodeStream(is);
+		}
+		is.close();
+
+    /*
+     * if the orientation is not 0 (or -1, which means we don't know), we
+     * have to do a rotation.
+     */
+		String type = context.getContentResolver().getType(photoUri);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+//		if (type.equals("image/png")) {
+//			srcBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+//		} else
+
+		if (type.equals("image/jpg") || type.equals("image/jpeg")) {
+			srcBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+		}
+		byte[] bMapArray = baos.toByteArray();
+		baos.close();
+		return BitmapFactory.decodeByteArray(bMapArray, 0, bMapArray.length);
+	}
+
+
 
 }
